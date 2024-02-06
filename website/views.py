7179a2a43505e3c5396a9f0b7f040ed4ead8d0a8
kevalvavaliya <kevalvavaliya@gmail.com>
2024-02-07 00:49:30 +0530

    project updates and minor fixes

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ def index(request):
     # about
     data[
         "aboutpara1"
-    ] = "I’m Keval, Currently pursuing a Bachelor of Engineering in Information Technology at LDCE, I consider myself as a maker of things, who solves user-problems using technology. My interests and skills extend to Machine Learning, Mobile apps development and System Design, allowing me to explore the realms of intelligent systems and architecting efficient solutions."
+    ] = "I’m Keval, a Software Developer from Surat, India.I am Currently pursuing Bachelor of Engineering in Information Technology at LDCE, I consider myself as a maker of things, who solves user-problems using technology. My interests and skills extend to  Mobile apps development,Backend systems and Machine Learning, allowing me to explore the realms of intelligent systems and architecting efficient solutions."
     data[
         "aboutpara2"
     ] = "I like to work with new tools and technologies,I’m always down for hearing about new projects, so feel free to drop me a line."
@@ -34,13 +34,13 @@ def index(request):
     }
     data["journey3"] = {
         "startyear": "2021",
-        "endyear": "",
-        "desc": "Started Bachelor in Information Technology,at LDCE , Ahmedabad",
+        "endyear": "2023",
+        "desc": "Bachelor in I.T, at LDCE,Hackathons, built projects like Yog4life and Plastidive",
     }
     data["journey4"] = {
-        "startyear": "2022-",
+        "startyear": "2023-",
         "endyear": "Present",
-        "desc": "Looking for gaining more skills/knowledge and tech job",
+        "desc": "Gained experience by working at startups like Discoze,Opensort and Kwiktech",
     }
 
     # my skills
@@ -82,6 +82,14 @@ def index(request):
     data["projects"] = [
         {
             "id": "01",
+            "name": "Vedvaani",
+            "live":"https://play.google.com/store/apps/details?id=com.vedvaani.app&pcampaignid=web_share",
+            "projectimg": "image/project/kevalvavaliya-vedvaani.png",
+            "tech": "#FLUTTER #MATERIALUI #FIREBASE",
+            "desc": "VedVaani is a platform that allows users to engage in conversations with AI characters. Users can interact with AI Astrologers, Movie Characters, Career and Love Coaches, and Influencers, and even participate in an Adventure Game.This app has been Designed, Developed and engineered by me at Discoze.",
+        },
+        {
+            "id": "02",
             "name": "Plastidive",
             "github": "https://github.com/kevalvavaliya/PlastiDive",
             "devpost":"https://devpost.com/software/plastdive",
@@ -91,7 +99,7 @@ def index(request):
             "desc": "A game app that helps to eliminate plastic,Click some polluted photos of the beach, the app will upload them to the server. YOLO model will determine the level of plastic and show it up on the map for help from society. Virtuous will help clean the place and upload the photo. YOLO model will check if the beach is clean or not and will reward virtuous with virtual currency.",
         },
         {
-            "id": "02",
+            "id": "03",
             "name": "Yog4Life Social App",
             "github": "https://github.com/kevalvavaliya/Yog4Life",
             "devfolio":"https://devfolio.co/projects/yoglife-b20d",
@@ -101,7 +109,7 @@ def index(request):
             "desc": "An app for helping people their healthy life. Yog4Life Has a feed feature which is help to get information from the folks, Yog4Life has an anonymous chat room to get connected with the world.",
         },
         {
-            "id": "03",
+            "id": "02",
             "name": "OpenSort's site",
             "github": "https://github.com/rajmahadev8/OpenSort",
             "live": "https://www.opensort.io/",
@@ -110,7 +118,7 @@ def index(request):
             "desc": "Designed and developed the website for a dynamic startup committed to family empowerment.",
         },
         {
-            "id": "04",
+            "id": "03",
             "name": "Bauddhik-Geeks Portfolio",
             "github": "https://github.com/Bauddhik-Geeks/Bauddhik-Geeks.github.io",
             "live": "https://bauddhikgeeks.co/",
@@ -119,7 +127,7 @@ def index(request):
             "desc": "Awesome Community Portfolio website for Bauddhik-Geeks community",
         },
         {
-            "id": "05",
+            "id": "06",
             "name": "Connect",
             "github": "https://github.com/abhigoyani/connect1",
             "projectimg": "image/project/kevalvavaliya-connect.png",
@@ -127,7 +135,7 @@ def index(request):
             "desc": "Connect connects audiences to all of your content with just one click.It is an open source alternative to linkrtree implemented in javscript and flask",
         },
         {
-            "id": "06",
+            "id": "07",
             "name": "Goverdhan Institute site",
             "github": "https://github.com/kevalvavaliya/Goverdhan-infotech",
             "projectimg": "image/project/kevalvavaliya-goverdhan.png",
@@ -135,7 +143,7 @@ def index(request):
             "desc": "Education site for goverdhan institute that manages the courses and events online which makes student easy to reach them.",
         },
         {
-            "id": "07",
+            "id": "08",
             "name": "We-Donate",
             "github": "https://github.com/kevalvavaliya/We-Donate",
             "projectimg": "image/project/kevalvavaliya-wedonate.png",
@@ -143,7 +151,7 @@ def index(request):
             "desc": "We-Donate aims in bringing digitization in donation.we donate delivers your donation to the right hands and provides home pickup services.",
         },
         {
-            "id": "08",
+            "id": "09",
             "name": "Localens",
             "github": "https://github.com/kevalvavaliya/Localens",
             "projectimg": "image/project/kevalvavaliya-localens.png",
@@ -151,20 +159,14 @@ def index(request):
             "desc": "Localens is an android app for finding local beauty spots in your localities and get them capture in the app and take it to the world",
         },
         {
-            "id": "09",
+            "id": "10",
             "name": "Vishvas Fabrics Ecommerce site",
             "github": "https://github.com/kevalvavaliya/E-commerce-site",
             "projectimg": "image/project/kevalvavaliya-vishvas.png",
             "tech": "#PHP #HTML #CSS #JS #MYSQL",
             "desc": "A basic ecommerce site for vishvas fabrics",
         },
-        {
-            "id": "10",
-            "name": "Sachivato news and blog site",
-            "projectimg": "image/project/itemcover.png",
-            "tech": "#WORDPRESS #DIGITALOCEAN",
-            "desc": "A basic News and Blog wordpress site",
-        },
+        
     ]
     data[
         "resumelink"
